68484710a3c1aa3cbc5d2db8c304dddbec4e7acd
engineeringawesome <robomower9508@gmail.com>
2015-11-19 23:07:41 -0500

    Added code for using the Arduino

--- app/src/main/java/com/lader11/ladder11/MainActivity.java
+++ app/src/main/java/com/lader11/ladder11/MainActivity.java
@@ -5,9 +5,26 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ArduinoBridge arduinoBridge;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arduinoBridge = new ArduinoBridge(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        arduinoBridge.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close the connection for safety
+        arduinoBridge.onPause();
     }
 }
