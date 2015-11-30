fb6619f917f2ce57c6ce9abba19da13ac6aea918
engineeringawesome <robomower9508@gmail.com>
2015-11-29 23:00:06 -0500

    added code to send the sync message for testing

--- app/src/main/java/com/lader11/ladder11/MainActivity.java
+++ app/src/main/java/com/lader11/ladder11/MainActivity.java
@@ -2,10 +2,16 @@ package com.lader11.ladder11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.hoho.android.usbserial.util.HexDump;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "Ladder11MainActivity";
 
     private ArduinoBridge arduinoBridge;
+    private int packetNum = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +32,19 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         //Close the connection for safety
         arduinoBridge.onPause();
+    }
+
+    public void sendSyncMsg(View v) {
+        packetNum++;
+        int command = 0;
+        byte[] packet = {   (byte) 0xFF,        //Start Byte
+                            (byte) 7,           //Size
+                            (byte) (packetNum>>8),
+                            (byte) packetNum,
+                            (byte) (command>>8),
+                            (byte) command,
+                            (byte) (0xFF+7+(packetNum>>8)+(packetNum&0xFF)+(command>>8)+(command&0xFF))};
+        Log.d(TAG, "Sending Packet ("+packet.length+"): "+HexDump.toHexString(packet));
+        arduinoBridge.sendBytes(packet);
     }
 }
