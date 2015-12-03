12e684bfcfde59d60bde8cb8f8e3a3b50b569749
engineeringawesome <robomower9508@gmail.com>
2015-12-02 22:47:42 -0500

    Does weird stuff where the thread wont end on time...

--- app/src/main/java/com/lader11/ladder11/MainActivity.java
+++ app/src/main/java/com/lader11/ladder11/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 import com.hoho.android.usbserial.util.HexDump;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private String TAG = "Ladder11MainActivity";
 
@@ -40,10 +42,30 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                Log.d(TAG, "Hello world");
-                try { Thread.sleep(1000); }
-                catch (InterruptedException e) {}
-                Log.d(TAG, "This is a test");
+                byte[] data = myLinx.buildPacket(0);
+                Log.d(TAG, "Sending packet");
+                arduinoBridge.sendBytes(data);
+                //Wait for packet receipt
+                //#TODO make a secondary storage space for the incoming data, since the packet is not recieved as one packet
+                long timeout = 250;     //Amount of milliseconds until waiting for a read times out
+                long startTime = System.currentTimeMillis();
+                ArrayList<Byte> packet = new ArrayList<>();
+                byte[] buffer = new byte[32];
+                int bytesRead;
+                while((System.currentTimeMillis()-startTime) < timeout) {
+                    Log.d(TAG, "Loop Time: "+(System.currentTimeMillis()-startTime)+": "+((System.currentTimeMillis()-startTime) < timeout));
+                    bytesRead = arduinoBridge.readBytes(buffer, 10);
+                    if(bytesRead > 0) {
+                        //Add the bytes to the list
+                        Log.d(TAG, "Initial list size: "+packet.size());
+                        Log.d(TAG, "Number of bytes read: "+bytesRead);
+                        for(int i=0; i<bytesRead; i++) {
+                            packet.add(buffer[i]);
+                        }
+                        Log.d(TAG, "New list: "+packet.toString());
+                    }
+                }
+                Log.d(TAG, "Done reading");
             }
         }).start();
     }
