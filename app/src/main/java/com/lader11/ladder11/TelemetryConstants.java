b49d7069e4eccffe8737c2c3f275af792b5c90cb
engineeringawesome <robomower9508@gmail.com>
2015-12-10 01:49:42 -0500

    Some initial constants and stuff

---
+++ app/src/main/java/com/lader11/ladder11/TelemetryConstants.java
@@ -0,0 +1,13 @@
+package com.lader11.ladder11;
+
+/**
+ * Created by jordanbrobots on 12/10/15.
+ */
+public class TelemetryConstants {
+    static final byte COMMAND_START = 0x01;
+    static final byte COMMAND_STOP = 0x02;
+    static final byte COMMAND_ROBOT_POSE = 0x03;
+    static final byte COMMAND_FLAME_LOCATION = 0x04;
+    static final byte COMMAND_CAMERA_DATA = 0x05;
+    static final byte COMMAND_BATT_VOLT = 0x06;
+}
