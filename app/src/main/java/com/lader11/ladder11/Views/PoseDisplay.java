01acb15acb07bff804fed583c8a1bbfdf11c7bbb
engineeringawesome <robomower9508@gmail.com>
2015-12-15 12:39:53 -0500

    Split pose information onto separate lines, and added bubble shadows

--- app/src/main/java/com/lader11/ladder11/Views/PoseDisplay.java
+++ app/src/main/java/com/lader11/ladder11/Views/PoseDisplay.java
@@ -25,6 +25,14 @@ public class PoseDisplay extends View {
     private int circleColor = 0xFF0000;     //Default to red
     private int labelColor = 0x000000;      //Default to black
     private String labelText = "";  //Default to empty
+    private String labelXText = "X: 000.00\"";
+    private float labelXHeight;
+    private String labelYText = "Y: 000.00\"";
+    private float labelYHeight;
+    private String labelOdoText = "Head: 345.67\u00b0";
+    private float labelOdoHeight;
+    private String labelGyroText = "Gyro: 234.57\u00b0";
+    private float labelGyroHeight;
     private int odoBubbleColor = 0xFF0000;
     private int gyroBubbleColor = 0x00FF00;
 
@@ -55,9 +63,7 @@ public class PoseDisplay extends View {
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.PoseDisplay, 0, 0);
 
         try {
-            //get the text and colors specified in the layout
-            labelText = a.getString(R.styleable.PoseDisplay_circleLabel);
-            labelText = "X: 153.56\"\n\rY: 234.13\"\n\rHead: 356.23\u00b0\n\rGyro: 123.45\u00b0";
+            //get the colors specified in the layout
             circleColor = a.getColor(R.styleable.PoseDisplay_circleColor, 0x0000FF);
             labelColor = a.getColor(R.styleable.PoseDisplay_textColor, 0);
             odoBubbleColor = a.getColor(R.styleable.PoseDisplay_odoBubbleColor, odoBubbleColor);
@@ -83,14 +89,18 @@ public class PoseDisplay extends View {
         mTextPaint.setTextSize(50);
 
         //Set the attributes for the circle
-        mCirclePaint.setStyle(Paint.Style.FILL);
+        mCirclePaint.setStyle(Paint.Style.STROKE);
         mCirclePaint.setColor(circleColor);
+        mCirclePaint.setStrokeWidth(8);
 
-        //Set the attributes for teh bubbles
+        //Set the attributes for the bubbles
         mOdoPaint.setStyle(Paint.Style.FILL);
         mOdoPaint.setColor(odoBubbleColor);
-        mGyroPaint.setStyle(Paint.Style.FILL);
+        mOdoPaint.setShadowLayer(3, 0, 3, R.color.shadow);
+        mGyroPaint.setStyle(Paint.Style.STROKE);
         mGyroPaint.setColor(gyroBubbleColor);
+        mGyroPaint.setStrokeWidth(7);
+        mGyroPaint.setShadowLayer(3,0,3,R.color.shadow);
     }
 
     @Override
@@ -109,9 +119,18 @@ public class PoseDisplay extends View {
 
         //Update bubble positions
         calculateBubblePositions();
+
+        //Calculate the positions of the label text
+        //Ascent returns - values, and descent returns + values.  Y values increase down the screen
+        labelXHeight = viewHeightHalf + 2*mTextPaint.ascent() - mTextPaint.descent();
+        labelYHeight = viewHeightHalf + mTextPaint.ascent();
+        labelOdoHeight = viewHeightHalf + mTextPaint.descent();
+        labelGyroHeight = viewHeightHalf - (2*mTextPaint.ascent()) + mTextPaint.descent();
     }
 
     private void calculateBubblePositions() {
+        //#TODO fix cos stuff to work with positive y moves down the screen
+        //#TODO find out if the bubbles should be on the line
         float hLength = radius + bubbleSpace/2;
         double dradius = (double) radius;
         odoX = (float) (viewWidthHalf + (Math.cos(Math.toRadians(-odoHead-90.0f)) * dradius));
@@ -126,7 +145,10 @@ public class PoseDisplay extends View {
         canvas.drawCircle(viewWidthHalf, viewHeightHalf, radius, mCirclePaint);
 
         //Draw the text!
-        canvas.drawText(labelText, viewWidthHalf, viewHeightHalf, mTextPaint);
+        canvas.drawText(labelXText, viewWidthHalf, labelXHeight, mTextPaint);
+        canvas.drawText(labelYText, viewWidthHalf, labelYHeight, mTextPaint);
+        canvas.drawText(labelOdoText, viewWidthHalf, labelOdoHeight, mTextPaint);
+        canvas.drawText(labelGyroText, viewWidthHalf, labelGyroHeight, mTextPaint);
 
         //Draw the heading bubbles
         canvas.drawCircle(gyroX, gyroY, bubbleSpace, mGyroPaint);
@@ -135,12 +157,14 @@ public class PoseDisplay extends View {
 
     public void setGyroHeading(float heading) {
         gyroHead = heading;
+        labelGyroText = String.format("Gyro: %f.2\u00b0", gyroHead);
         calculateBubblePositions();
         invalidate();
     }
 
     public void setOdometryHeading(float heading) {
         odoHead = heading;
+        labelOdoText = String.format("Head: %f.2\u00b0", odoHead);
         calculateBubblePositions();
         invalidate();
     }
@@ -148,6 +172,8 @@ public class PoseDisplay extends View {
     public void setHeadings(float odometryHeading, float gyroHeading) {
         odoHead = odometryHeading;
         gyroHead = gyroHeading;
+        labelOdoText = String.format("Head: %f.2\u00b0", odoHead);
+        labelGyroText = String.format("Gyro: %f.2\u00b0", gyroHead);
         calculateBubblePositions();
         invalidate();
     }
@@ -155,6 +181,8 @@ public class PoseDisplay extends View {
     public void setPosition(float x, float y) {
         xPos = x;
         yPos = y;
+        labelXText = String.format("X: %f.2\"", xPos);
+        labelYText = String.format("Y: %f.2\"", yPos);
         invalidate();
     }
 
@@ -162,8 +190,11 @@ public class PoseDisplay extends View {
         xPos = x;
         yPos = y;
         odoHead = odometryHeading;
+        labelXText = String.format("X: %f.2\"", xPos);
+        labelYText = String.format("Y: %f.2\"", yPos);
+        labelOdoText = String.format("Head: %f.2\u00b0", odoHead);
         calculateBubblePositions();
-        labelText = x+", "+y;
+        //labelText = x+", "+y;
         invalidate();
     }
 
